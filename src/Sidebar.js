08b997c21e8294bc0833fd4322bef91cb198f1db
1js2002 <dgoud99120@gmail.com>
2023-03-05 23:40:11 +0530

    finished sidebarcss

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,16 @@ import React from 'react'
 import './Sidebar.css'
 
 function Sidebar() {
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+          <span className="sidebar__hash">#</span>
+          <p>{topic}</p>
+        </div>
+      );
   return (
     <div className="sidebar">
         <div className="sidebar__top">
-            <img src="" alt="" />
+            <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80" alt="" />
             <Avatar className="sidebar__avatar"/>
             <h3>Dinesh</h3>
             <h4>dgoud99120@gmail.com</h4>
@@ -24,6 +30,11 @@ function Sidebar() {
         </div>
         <div className="sidebar__bottom">
             <p>Recent</p>
+            {recentItem("reactjs")}
+            {recentItem("programming")}
+            {recentItem("softwareengineering")}
+            {recentItem("design")}
+            {recentItem("developer")}
         </div>
     </div>
   );
